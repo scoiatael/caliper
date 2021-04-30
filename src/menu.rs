5b8049391d69417ec1bac2d0f38e2c230f14e684
Lukasz Czaplinski <git@scoiatael.dev>
2021-04-30 20:00:40 +0200

    Release v0.2.1 - fixed icons

--- src/menu.rs
+++ src/menu.rs
@@ -1,3 +1,4 @@
+use iced::widget::svg::Handle;
 use iced::{button, Button, Element, Length, Row, Svg};
 
 #[derive(Default)]
@@ -17,44 +18,65 @@ pub enum Message {
     Save,
 }
 
-fn icon<A>(name: &str) -> Element<A> {
-    Svg::from_path(format!(
-        "{}/resources/fontawesome-free-5.15.3-desktop/svgs/{}.svg",
-        env!("CARGO_MANIFEST_DIR"),
-        name
-    ))
-    .width(Length::Units(20))
-    .height(Length::Units(20))
-    .into()
+fn icon<A>(bytes: &[u8]) -> Element<A> {
+    Svg::new(Handle::from_memory(bytes))
+        .width(Length::Units(20))
+        .height(Length::Units(20))
+        .into()
 }
 
 impl State {
     pub fn view(&mut self) -> Element<Message> {
         Row::new()
             .push(
-                Button::new(&mut self.save, icon("solid/save"))
-                    .padding(8)
-                    .on_press(Message::Save),
+                Button::new(
+                    &mut self.save,
+                    icon(include_bytes!(
+                        "fontawesome-free-5.15.3-desktop/svgs/solid/save.svg"
+                    )),
+                )
+                .padding(8)
+                .on_press(Message::Save),
             )
             .push(
-                Button::new(&mut self.open, icon("solid/folder-open"))
-                    .padding(8)
-                    .on_press(Message::Clear),
+                Button::new(
+                    &mut self.open,
+                    icon(include_bytes!(
+                        "fontawesome-free-5.15.3-desktop/svgs/solid/folder-open.svg"
+                    )),
+                )
+                .padding(8)
+                .on_press(Message::Clear),
             )
             .push(
-                Button::new(&mut self.clear, icon("solid/recycle"))
-                    .padding(8)
-                    .on_press(Message::Clear),
+                Button::new(
+                    &mut self.clear,
+                    icon(include_bytes!(
+                        "fontawesome-free-5.15.3-desktop/svgs/solid/recycle.svg"
+                    )),
+                )
+                .padding(8)
+                .on_press(Message::Clear),
             )
             .push(
-                Button::new(&mut self.export, icon("solid/file-export"))
-                    .padding(8)
-                    .on_press(Message::Export),
+                Button::new(
+                    &mut self.export,
+                    icon(include_bytes!(
+                        "fontawesome-free-5.15.3-desktop/svgs/solid/file-export.svg"
+                    )),
+                )
+                .padding(8)
+                .on_press(Message::Export),
             )
             .push(
-                Button::new(&mut self.side_bar, icon("solid/list"))
-                    .padding(8)
-                    .on_press(Message::Sidebar),
+                Button::new(
+                    &mut self.side_bar,
+                    icon(include_bytes!(
+                        "fontawesome-free-5.15.3-desktop/svgs/solid/list.svg"
+                    )),
+                )
+                .padding(8)
+                .on_press(Message::Sidebar),
             )
             .into()
     }
